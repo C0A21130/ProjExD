45426e1fc5c2df0b9c47df3c4b8477668935f25e
yasuno <c0a2113061@edu.teu.ac.jp>
2022-06-14 17:30:20 +0900

    編集：コメントの追加

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,25 +1,26 @@
 from random import randint
 
-taget_string_num = 5
-lost_string_num = 3
+TARGET_STRING_NUM = 5 # 表示文字数
+LOST_STRING_NUM = 3   # 欠損文字数
 
 def main():
-    taget_strings = []
-    for i in range(taget_string_num):
-        rand = randint(65,90) # 65(A) ~ 90(Z)の乱数を作成
+    taget_strings = [] # 対象文字を保存するリスト
+    for i in range(TARGET_STRING_NUM):
+        rand = randint(65,90) # 65(A)~90(Z)の乱数を作成
         taget_strings.append(rand)
 
-    lost_strings = []
-    for i in range(lost_string_num):
-        rand = 0
+    lost_strings = [] # 欠損文字を保存するリスト
+    for i in range(LOST_STRING_NUM):
+        rand = 0 # 乱数を保存する変数
         while (True):
-            rand = randint(65, 90)
+            rand = randint(65, 90) # 65(A)~90(Z)
             if (rand not in taget_strings):
                 break 
         lost_strings.append(rand)
 
     print("対象文字")
-    for i in taget_strings:
+    # 対象文字の表示後、欠損文字の表示
+    for i in taget_strings: 
         print("{:3s}".format(chr(i)), end="")
     for i in lost_strings:
         print("{:3s}".format(chr(i)), end="")
@@ -45,19 +46,24 @@ def main():
             print("-", end="")
         print("")
 
+# 欠損文字の入力を求めて実際の文字と適合しているか確認する関数
 def string_ans(lost_strings):
     ansers = []
-    for i in range(lost_string_num):
+    # 実際の欠損文字数の分の文字数の入力を求める
+    for i in range(LOST_STRING_NUM):
         i = input(f"{i+1}もじめは?")
         ansers.append(ord(i))
 
+    # 欠損文字にヒットする度に1足していく
     status = 0
     for a in ansers:
         if (a in lost_strings):
             status += 1
         else:
             continue
-    if (status == lost_string_num):
+
+    # 実際に欠損した文字数と入力してヒットした文字数が一致したときに正解を返す
+    if (status == LOST_STRING_NUM):
         print("正解!!")
     else:
         print("不正解です。またチャレンジしてください")
