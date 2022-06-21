ecb33c7b5cb1963527b2c07a7935b165cf6856cb
yasuno <c0a2113061@edu.teu.ac.jp>
2022-06-21 15:27:23 +0900

    練習問題7

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,28 +5,35 @@ import tkinter.messagebox as tkm
 
 
 def main():
-
+    
+    # ボタン以外を入力したときの動作
     def button_click(event):
         btn = event.widget
         txt = btn["text"]
-        entry.insert(tk.END, int(txt))
-        
-    def plus_button_click(event):
-        btn = event.widget
-        txt = btn["text"]
-        tkm.showwarning("",f"{txt}")
+        if txt == "+":
+            entry.insert(tk.END, "+")
+        elif txt == "=":
+            formula = entry.get()
+            result = eval(formula)
+            entry.delete(0, tk.END)
+            entry.insert(tk.END, int(result))
+        else:
+            entry.insert(tk.END, int(txt))
     
+
     # tkクラスからアプリの基本となるインスタンスを作成
     root = tk.Tk()
     root.title("計算機")
     root.geometry("300x600")
 
+    # 数字を表示する画面を作成する
     entry = tk.Entry(justify="right",
                     width=10,
                     font=("Times New Roman",40)
                     )
     entry.grid(row=0, column=0, columnspan=3)
     
+    # 数字のボタンを9~0まで作成する
     r = 1
     c = 0
     for i in range(9, -1, -1):
@@ -44,6 +51,7 @@ def main():
         button.grid(row=r, column=c)
         button.bind("<1>", button_click)
     
+    # プラスボタンを作成する
     plus_button = tk.Button(root,
                         width=4,
                         height=2,
@@ -51,7 +59,16 @@ def main():
                         font=("Times New Roman", 30)
                         )
     plus_button.grid(row=5, column=1)
-    plus_button.bind("<1>", plus_button_click)
+    plus_button.bind("<1>", button_click)
+
+    equal_button = tk.Button(root,
+                        width=4,
+                        height=2,
+                        text="=",
+                        font=("Times New Roman", 30)
+                        )
+    equal_button.grid(row=5, column=2)
+    equal_button.bind("<1>", button_click)
 
     root.mainloop()
 
