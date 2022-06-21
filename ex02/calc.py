ebf1fe6a59ddf8e2b6a62ff5db45692e11645231
yasuno <c0a2113061@edu.teu.ac.jp>
2022-06-21 14:35:35 +0900

    練習問題2

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,6 +6,23 @@ def main():
     root = tk.Tk()
     root.title("計算機")
     root.geometry("300x500")
+    
+    r = 0
+    c = 0
+    for i in range(9, -1, -1):
+        button = tk.Button(root,
+                        width=4,
+                        height=2,
+                        text=i,
+                        font=("Times New Roman", 30)
+                        )
+        if (i%3==0):
+            r += 1
+            c = 0
+        else:
+            c += 1
+        button.grid(row=r, column=c)
+    
     root.mainloop()
 
 if __name__ == "__main__":
