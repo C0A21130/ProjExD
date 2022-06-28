d5c3811bb274fd71e695e34f383cfa1a0f99667c
yasuno <c0a2113061@edu.teu.ac.jp>
2022-06-28 14:19:32 +0900

    練習問題2

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,6 +4,12 @@ from sympy import im
 import tkinter as tk
 
 if __name__ == "__main__":
+    # ウィンドウの生成
     root = tk.Tk()
     root.title("迷えるこうかとん")
+    root.geometry("1500x900")
+
+    # canvasの生成
+    canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+    canvas.pack()
     root.mainloop()
