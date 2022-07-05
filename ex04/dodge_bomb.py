b5689c3347d37df73efb80ec1a5fca6e6bad4f78
yasuno <c0a2113061@edu.teu.ac.jp>
2022-07-05 16:52:58 +0900

    4限追加機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,8 +3,10 @@ import pygame as pg
 import sys
 
 def main():
+    # 秒数をclock変数に保存する
     clock = pg.time.Clock()
 
+    # スクリーンを表示する
     pg.display.set_caption("逃げ！こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900))
     screen_rct = screen_sfc.get_rect()
@@ -22,17 +24,56 @@ def main():
     kkimg_sfc.blit(kkimg_sfc, kkimg_rct)
 
     # 爆弾
-    bom_sfc = pg.Surface((20, 20))
+    bsize = 20
+    bom_sfc = pg.Surface((bsize, bsize))
     bom_sfc.set_colorkey((0, 0, 0))
-    pg.draw.circle(bom_sfc, (255, 0, 0), (10, 10), 10)
+    pg.draw.circle(bom_sfc, (255, 0, 0), (bsize/2, bsize/2), bsize/2)
     bom_rct = bom_sfc.get_rect()
     bom_rct.centerx = random.randint(0, screen_rct.width)
     bom_rct.centery = random.randint(0, screen_rct.height)
     vx, vy = +1, +1
 
+    # ポイント(0:表示しない、1:ポイント表示かつゲットしていない、2：ポイント取得後)
+    point = 0
+    comment = "" # スターを取得するとクリアしたときにSPECIAL CLEARに変化する
+
+    # スター
+    star_sfc = pg.Surface((100, 100))
+    star_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(star_sfc, (255, 255, 0), (50, 50), 50)
+    star_rct = star_sfc.get_rect()
+    rx = random.randint(0, screen_rct.width)
+    ry = random.randint(0, screen_rct.height)
+    star_rct.centerx = rx
+    star_rct.centery = ry
+    # スターにP
+    s_fonto = pg.font.Font(None, 100)
+    s_txt = s_fonto.render("P", True, (30, 30, 30))
+
     while True:
+        # 背景を表示する
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
+        # 秒数を表示する
+        fonto = pg.font.Font(None, 80)
+        txt = fonto.render(str(pg.time.get_ticks()/1000), True, (0, 0, 0))
+        screen_sfc.blit(txt, (50, 50))
+
+        # 30秒経過したらクリアと表示する
+        if int(pg.time.get_ticks()) > 10000:
+            print("HElLO")
+            Cfonto = pg.font.Font(None, 80)
+            Ctxt = Cfonto.render(f"{comment}CLEAR", True, (0, 0, 0))
+            screen_sfc.blit(Ctxt, (50, 150))
+        # 1秒ごとに爆弾が大きくなる
+        if((int(pg.time.get_ticks()) % 1000) < 100):
+            bsize += 0.5
+            bom_sfc = pg.Surface((bsize, bsize))
+        # 5秒経過後スターが表示される
+        if(int(pg.time.get_ticks()) > 5000 and point==0):
+            point = 1 
+            
+        # ×を押したときに終了する
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return 
@@ -50,21 +91,29 @@ def main():
             if key_status[pg.K_LEFT]  == True: kkimg_rct.centerx += 1
             if key_status[pg.K_RIGHT] == True: kkimg_rct.centerx -= 1
         if kkimg_rct.colliderect(bom_rct):
-            return 
+            return
+        if kkimg_rct.colliderect(star_rct):
+            point=2
             
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
         # 爆弾を移動させる
         bom_rct.move_ip(vx, vy)
 
+        # 爆弾が壁にぶつかったとき反射する
         yoko, tate = check_bound(bom_rct, screen_rct)
         vx *= yoko
         vy *= tate
 
         # 爆弾を生成する
         screen_sfc.blit(bom_sfc, bom_rct)
-        
-        
+
+        # 1の時にスターを表示、2:コメントを加える
+        if point == 1:
+            screen_sfc.blit(star_sfc, star_rct)    # スターを表示する
+            screen_sfc.blit(s_txt, (rx-15, ry-27)) # 文字Pを表示する
+        if point == 2:
+            comment = "SPECIAL"
 
         pg.display.update()
         clock.tick(1000)
