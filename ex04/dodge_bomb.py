51070892873b89bc849c454775f41cbde4cd679b
yasuno <c0a2113061@edu.teu.ac.jp>
2022-07-05 15:31:07 +0900

    3限基本機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -44,10 +44,19 @@ def main():
         if key_status[pg.K_DOWN]  == True: kkimg_rct.centery += 1
         if key_status[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
         if key_status[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
+        if check_bound(kkimg_rct, screen_rct) != (1, 1):
+            if key_status[pg.K_UP]    == True: kkimg_rct.centery += 1
+            if key_status[pg.K_DOWN]  == True: kkimg_rct.centery -= 1
+            if key_status[pg.K_LEFT]  == True: kkimg_rct.centerx += 1
+            if key_status[pg.K_RIGHT] == True: kkimg_rct.centerx -= 1
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
         # 爆弾を移動させる
         bom_rct.move_ip(vx, vy)
+
+        yoko, tate = check_bound(bom_rct, screen_rct)
+        vx *= yoko
+        vy *= tate
 
         # 爆弾を生成する
         screen_sfc.blit(bom_sfc, bom_rct)
@@ -55,6 +64,15 @@ def main():
         pg.display.update()
         clock.tick(1000)
 
+
+def check_bound(rct, scr_rct):
+
+    yoko, tate = +1, +1
+    if rct.left < scr_rct.left or scr_rct.right < rct.right:
+        yoko = -1 # 範囲外
+    if rct.top < scr_rct.top or scr_rct.bottom < rct.bottom:
+        tate = -1 # 範囲外
+    return yoko, tate
 
 
 if __name__ == "__main__":
